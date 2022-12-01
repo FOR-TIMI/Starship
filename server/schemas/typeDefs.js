773fc12fd2012f03d4c2aed745721cae3802bc7b
Jacob Adelman <jacobzadelman@gmail.com>
2022-12-01 18:15:43 -0500

    fix typdefs issue

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -112,20 +112,19 @@ const typeDefs = gql`
     getDataFromBasket(id: ID!): [Bardata]
   }
 
- type Mutation {
-      login(email: String!, password: String!): Auth
-      addUser(username: String!, email: String!, password: String!): Auth
-      addTicker(basketId: ID!, ticker: String!): Basket
-      addBasket(tickerId: ID!):Basket
-      addPost(title: String!): Post
-      addComment(postId: ID!, commentText: String!): Post 
-      addFollowing(followingId: ID!): User
-      removeFollowing(followingId: ID!): User
-      addLike(postId: ID!): Like
-      deleteBasket(basketId: ID!): Basket
-
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addTicker(basketId: ID!, ticker: String!): Basket
+    addBasket(tickerId: ID!): Basket
+    addPost(title: String!): Post
+    addComment(postId: ID!, commentText: String!): Post
+    addFollowing(followingId: ID!): User
+    removeFollowing(followingId: ID!): User
+    addLike(postId: ID!): Like
+    deleteBasket(basketId: ID!): Basket
   }
-
+`;
 
 //addBasket(basket:[Basket] ): Basket
 //exporting the typeDefs
