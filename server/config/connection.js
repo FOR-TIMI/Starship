033b85d73f2de0edd46ea72b6c87a33f5d1de8e4
FOR-TIMI <timiplayskeys@gmail.com>
2022-12-03 05:54:45 -0500

    updated blog route to tavern

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/starship', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
 
 module.exports = mongoose.connection;
