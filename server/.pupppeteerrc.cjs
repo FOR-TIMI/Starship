601673d5e19aadd51ff0d52c392235b92f8b608b
Jacob Adelman <jacobzadelman@gmail.com>
2022-12-06 22:45:01 -0500

    test

--- server/.pupppeteerrc.cjs
+++ server/.pupppeteerrc.cjs
@@ -1,9 +1,5 @@
 const { join } = require("path");
 
-/**
- * @type {import("puppeteer").Configuration}
- */
 module.exports = {
-  // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, ".cache", "puppeteer"),
 };
