f8133008688c8dbd1f4be2e19704a8199104f544
Jacob Adelman <jacobzadelman@gmail.com>
2022-12-06 20:35:51 -0500

    changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
-const DB_URL = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/starship';
+const mongoose = require("mongoose");
+const DB_URL = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/starship";
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
