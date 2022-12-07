396554e1fca1b73bbe28292e19ee27344aacb18c
Jacob Adelman <jacobzadelman@gmail.com>
2022-12-07 02:05:42 -0500

    working deployment

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 }
