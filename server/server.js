ddb0dcffafe882da9870c95545a56ceb2ce70075
Jacob Adelman <jacobzadelman@gmail.com>
2022-12-07 01:30:27 -0500

    change server

--- server/server.js
+++ server/server.js
@@ -28,6 +28,10 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/client/build/index.html"));
+  });
 }
 
 // Create a new instance of an Apollo server with the GraphQL schema
