7fc6d21eb9f3d9d4cc2a1916040bdead774da7cf
Jacob Adelman <jacobzadelman@gmail.com>
2022-12-07 01:47:02 -0500

    static json changes

--- server/server.js
+++ server/server.js
@@ -27,11 +27,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  // app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  // });
 }
 
 // Create a new instance of an Apollo server with the GraphQL schema
