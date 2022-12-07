8cab0b7fd4bd8a4d4efd6c5e50cefee8d7cf370f
Jacob Adelman <jacobzadelman@gmail.com>
2022-12-07 01:37:28 -0500

    remove static

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  // app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/client/build/index.html"));
