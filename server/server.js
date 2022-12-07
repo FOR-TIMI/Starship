951a939731312402da07d38e70d85bd1fdd80860
Jacob Adelman <jacobzadelman@gmail.com>
2022-12-06 20:07:04 -0500

    add path

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require("./config/connection");
 const app = express();
+const path = require("path");
 
 //Port
 const PORT = process.env.PORT || 3001;
